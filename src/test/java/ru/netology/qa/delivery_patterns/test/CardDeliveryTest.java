4b8b9669361b6d4eae0ada635cd37c56eeee01d9
Konysheva Tatyana <tkonyseva693@gmail.com>
2023-07-27 11:22:31 +0300

    Added branch and allure

--- src/test/java/ru/netology/qa/delivery_patterns/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/qa/delivery_patterns/test/CardDeliveryTest.java
@@ -1,5 +1,9 @@
 package ru.netology.qa.delivery_patterns.test;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.qa.delivery_patterns.data.DataGenerator;
@@ -14,6 +18,15 @@ import static com.codeborne.selenide.Selenide.open;
 
 
 public class CardDeliveryTest {
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @Test
     void shouldCardDeliverySuccess() {
